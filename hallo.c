1291a280d0605e50169fe0e2e9f1a2f89da0397d
Jakob <70209576+DrKarak@users.noreply.github.com>
2021-12-17 09:58:47 +0100

    Exploit und verwundbares Programm

---
+++ hallo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void check(char *buf){
+    for(char c = *buf++; c != '\x00'; c = *buf++) {
+        if(c=='A') {
+            printf("Hacker blockiert!\n");
+            exit(-1);
+        }
+    }
+}
+
+void gruss() {
+    char buf[256] = {0};
+    printf("\nWie ist dein Name?:\n");
+    gets(buf);
+    check(buf);
+    printf(buf);
+    printf(" ich gruesse dich!");
+}
+
+int main(int argc, char* argv[]) {
+    while(1) {
+        gruss();
+    }
+}
